700b8d40d3d13047c392d58616eff2aabb931228
BookShelf <seunghyeoks@naver.com>
2025-05-20 21:55:17 +0900

    fix assert

--- src/libFuzzer/cmd_dis_libfuzzer.cpp
+++ src/libFuzzer/cmd_dis_libfuzzer.cpp
@@ -1,4 +1,17 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "harness/CmdDispatcherHarness.hpp"
+
+void handle_abort(int sig) {
+    fprintf(stderr, "[fuzz] caught abort/assert (signal %d), continue fuzzing\n", sig);
+    // crash로 기록만 하고, 프로세스는 죽이지 않음
+}
+
+__attribute__((constructor))
+void setup_signal_handler() {
+    signal(SIGABRT, handle_abort);
+}
 
 // libFuzzer 퍼징 진입점
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* Data, size_t Size) {
